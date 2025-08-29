e1ea1d19e00382d017079015106fb98dbc71b537
Himanshu Kumar <rajh5343@gmail.com>
2025-08-29 17:21:29 +0530

    add url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1/students": "http://localhost:8000"
+      "/api/v1/students": "https://enquiry-form-backend-porf.onrender.com"
     }
   },
   plugins: [react(), tailwindcss()],
